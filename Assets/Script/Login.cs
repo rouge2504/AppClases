e758adc37ec945c20c274f1fe54d3eb61fa8ddda
LAPTOP-N0FT603V\rouge <rouge2504@gmail.com>
2020-07-07 11:36:35 -0500

    Clase del sabado

--- Assets/Script/Login.cs
+++ Assets/Script/Login.cs
@@ -15,10 +15,15 @@ public class Login : MonoBehaviour
     public string validacion_Password;
 
     private int validacion;
+
+
+    private Firebase.Auth.FirebaseAuth auth;
+    private Firebase.Auth.FirebaseUser user;
     // Start is called before the first frame update
     void Start()
     {
 
+        InitializeFirebase();
 
         validacion = PlayerPrefs.GetInt("Validacion");
         print(validacion);
@@ -60,4 +65,54 @@ public class Login : MonoBehaviour
 
 
     }
+
+    public void UsuarioNuevo()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(mail.text, password.text).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    void InitializeFirebase()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                /*DebugLog("Signed in " + user.UserId);
+                displayName = user.DisplayName ?? "";
+                emailAddress = user.Email ?? "";
+                photoUrl = user.PhotoUrl ?? "";*/
+            }
+        }
+    }
+
+
 }
