1ce83032c0945c4b655c8a37e5b177877a23d6c3
LAPTOP-N0FT603V\rouge <rouge2504@gmail.com>
2020-07-11 14:53:12 -0500

    Clase 11/07/2020

---
+++ Assets/Script/DataBaseManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using Firebase.Unity.Editor;
+using Firebase;
+using Firebase.Database;
+using Proyecto26;
+
+
+public class DataBaseManager : MonoBehaviour
+{
+    public DatabaseReference reference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://appclasse-f518c.firebaseio.com/");
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        User user = new User("Rojo", "rouge2504@gmail.com", "Hola como estas");
+
+        RestClient.Put("https://appclasse-f518c.firebaseio.com/"+ user.username +".json", user);
+        //RestClient.Put("https://appclasse-f518c.firebaseio.com/"+ user.username +".json", user);
+
+        /*RestClient.Get<User>("https://appclasse-f518c.firebaseio.com/" + user.username + ".json").Then(response =>
+        {
+            user = response;
+        });
+
+        print(user.username);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
+
+public class User
+{
+    public string username;
+    public string email;
+    public string menssage;
+
+    public User()
+    {
+    }
+
+    public User(string username, string email, string menssage)
+    {
+        this.username = username;
+        this.email = email;
+        this.menssage = menssage;
+    }
+}
--- Assets/Script/Login.cs
+++ Assets/Script/Login.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
+using Firebase.Unity.Editor;
+using Firebase;
+using Firebase.Database;
+using Proyecto26;
+
 public class Login : MonoBehaviour
 {
+    [Header("Registro")]
     public InputField nombre;
     public InputField apellido;
     public InputField mail;
     public InputField password;
+
+    [Header("Login")]
+    public InputField mailLogin;
+    public InputField passwordLogin;
+
+    public Text failText;
+
+    public bool checkText;
 
     public string validacion_Mail;
     public string validacion_Password;
@@ -19,25 +34,51 @@ public class Login : MonoBehaviour
 
     private Firebase.Auth.FirebaseAuth auth;
     private Firebase.Auth.FirebaseUser user;
+
+    private Usuario _user;
+
+    private bool usuarioNuevoLogeado;
+
     // Start is called before the first frame update
     void Start()
     {
 
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://appclasse-f518c.firebaseio.com/");
+
+        _user = new Usuario("Rogelio", "Trejo", "rouge2504@gmail.com", "admin12345678");
+
+        RestClient.Put("https://appclasse-f518c.firebaseio.com/"+ _user.nombre + ".json", _user);
+
+        
+        checkText = false;
+
+        usuarioNuevoLogeado = false;
+
         InitializeFirebase();
 
         validacion = PlayerPrefs.GetInt("Validacion");
+
+        string name = PlayerPrefs.GetString("nombre");
+        print(name);
+
         print(validacion);
         if (validacion == 1)
         {
             print("Acceso Concedido");
             SceneManager.LoadScene("Principal");
         }
+
+
+        /*_user = new Usuario("Rogelio", "Trejo", "rouge2504@gmail.com", "admin12345678");
+        _user.Guardar();*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        failText.gameObject.SetActive(checkText);
+
+        ChequeoNuevoUsuario();
     }
 
     public void CambiarEscena()
@@ -82,9 +123,56 @@ public class Login : MonoBehaviour
 
             // Firebase user has been created.
             Firebase.Auth.FirebaseUser newUser = task.Result;
+
+            usuarioNuevoLogeado = true;
+
+
+
             Debug.LogFormat("Firebase user created successfully: {0} ({1})",
                 newUser.DisplayName, newUser.UserId);
         });
+    }
+
+    void ChequeoNuevoUsuario()
+    {
+        if (usuarioNuevoLogeado)
+        {
+            _user = new Usuario(nombre.text, apellido.text, mail.text, password.text);
+            _user.Guardar();
+            usuarioNuevoLogeado = false;
+        }
+    }
+
+    public void LoginButton()
+    {
+        if (string.IsNullOrEmpty(mailLogin.text) == false || string.IsNullOrEmpty(passwordLogin.text) == false)
+        {
+            auth.SignInWithEmailAndPasswordAsync(mailLogin.text, passwordLogin.text).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    //Debug.LogError("Encotro un error: " + task.Exception);
+                    failText.text = "El mail o el password estan mal";
+                    checkText = true;
+                    
+                    return;
+                }
+
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+            });
+        }
+        else
+        {
+            failText.text = "Algunos de los campos esta vacio";
+            failText.gameObject.SetActive(true);
+            //print("Algunos de los campos esta vacio");
+        }
     }
 
     void InitializeFirebase()
@@ -115,4 +203,37 @@ public class Login : MonoBehaviour
     }
 
 
+}
+
+public class Usuario
+{
+    public string nombre;
+    public string apellido;
+    public string mail;
+    public string password;
+
+
+    public Usuario()
+    {
+
+    }
+
+    public Usuario(string _name, string _lastName, string _email, string _password)
+    {
+        nombre = _name;
+        apellido = _lastName;
+        mail = _email;
+        password = _password;
+
+    }
+
+    public void Guardar()
+    {
+        PlayerPrefs.SetString("nombre", nombre);
+        PlayerPrefs.SetString("apellido", apellido);
+        PlayerPrefs.SetString("mail", mail);
+        PlayerPrefs.SetString("password", password);
+
+        Debug.Log("variables guardadas");
+    }
 }
